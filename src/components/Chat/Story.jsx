d811b116efa25fd3f9a0fd178056a6caee45b9e2
AkashChakrabortty <akashchakrabortty@gmail.com>
2022-11-04 11:16:08 +0600

    story ui

--- src/components/Chat/Story.jsx
+++ src/components/Chat/Story.jsx
@@ -1,7 +1,19 @@
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 
 const Story = () => {
-  return <div>Story</div>;
+  return (
+    <div className="col-10 mx-auto row row-cols-2 justify-content-evenly mt-2">
+      <div className="setStory border rounded col-5 align-items-center d-flex justify-content-center">
+        <FontAwesomeIcon icon={faPlus} className="fs-1" />
+      </div>
+      <div
+        className="showStory border rounded col-5"
+        style={{ height: "200px" }}
+      ></div>
+    </div>
+  );
 };
 
 export default Story;
